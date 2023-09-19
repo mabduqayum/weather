620dfa999db0053238e28ebc3cfcdc8d84596c31
Ali <mabduqayum@gmail.com>
2023-09-20 00:27:16 +0500

    refactored

--- src/app/components/search-city/search-city.component.ts
+++ src/app/components/search-city/search-city.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { City } from '../../interfaces/city';
 import {
@@ -19,6 +27,7 @@ import { NgFor, NgIf } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'search-city',
@@ -42,38 +51,30 @@ export class SearchCityComponent implements OnInit, OnDestroy {
   citySearchControl: FormControl = new FormControl('');
   loading: boolean = false;
   searchCityOptions: City[] = [];
-  @Output() addCity: EventEmitter<City> = new EventEmitter<City>();
+  @Output() addCity = new EventEmitter<City>();
+  @ViewChild('cityInput') cityInput!: ElementRef;
 
-  private readonly destroy: Subject<void> = new Subject<void>();
+  private readonly destroy$ = new Subject<void>();
 
-  constructor(private cityService: CityService) {}
+  constructor(
+    private cityService: CityService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
-    this.citySearchControl.valueChanges
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
-        tap((_) => (this.searchCityOptions = [])),
-        filter((value) => value?.length > 2),
-        mergeMap((value: string) => {
-          this.loading = true;
-          return this.cityService.getCities(value);
-        }),
-        filter((cities: City[]) => {
-          this.loading = false;
-          return cities.length > 0;
-        }),
-        map((cities: City[]) => cities.filter((city: City) => city?.name)),
-        takeUntil(this.destroy),
-      )
-      .subscribe((cities: City[]): void => {
-        this.searchCityOptions = cities;
-      });
+    this.initQueryParams();
+    this.focusInput();
+    this.initCitySearch();
   }
 
   ngOnDestroy(): void {
-    this.destroy.next();
-    this.destroy.complete();
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  resetSearchControl(): void {
+    this.citySearchControl.reset();
   }
 
   selectCity(): void {
@@ -84,7 +85,57 @@ export class SearchCityComponent implements OnInit, OnDestroy {
     this.resetSearchControl();
   }
 
-  resetSearchControl(): void {
-    this.citySearchControl.reset();
+  async updateURL(cityName: string) {
+    if (cityName) {
+      await this.router.navigate([], {
+        queryParams: { city: cityName },
+        queryParamsHandling: 'merge',
+      });
+    } else {
+      const queryParams = { ...this.route.snapshot.queryParams };
+      delete queryParams['city'];
+      await this.router.navigate([], {
+        queryParams: queryParams,
+      });
+    }
+  }
+
+  private focusInput(): void {
+    setTimeout(() => this.cityInput.nativeElement.focus(), 1000);
+  }
+
+  private initCitySearch(): void {
+    this.citySearchControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap(async (cityName: string) => {
+          await this.updateURL(cityName);
+          this.searchCityOptions = [];
+        }),
+        filter((value) => value?.length > 2),
+        mergeMap((value: string) => {
+          this.loading = true;
+          return this.cityService.getCities(value);
+        }),
+        filter((cities: City[]) => {
+          this.loading = false;
+          return cities.length > 0;
+        }),
+        map((cities: City[]) => cities.filter((city: City) => city?.name)),
+        takeUntil(this.destroy$),
+      )
+      .subscribe((cities: City[]): void => {
+        this.searchCityOptions = cities;
+      });
+  }
+
+  private initQueryParams(): void {
+    this.route.queryParams.subscribe((params) => {
+      const city = params['city'];
+      if (city) {
+        this.citySearchControl.setValue(city);
+      }
+    });
   }
 }
